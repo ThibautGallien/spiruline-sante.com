d60a2709dddefd3fc3d233801ea6eafa4f836d9c
Thibaut Gallien <131788305+ThibautGallien@users.noreply.github.com>
2025-07-15 15:53:50 +0200

    Fix next-intl plugin import path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withNextIntl = require('@next-intl/plugin')('./lib/i18n.ts');
+const withNextIntl = require('next-intl/plugin')('./lib/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
